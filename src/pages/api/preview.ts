a2ed9ebb18d3971e010b5972d37328aa9dc31073
Gabriel Souza <gabrielcsg19@hotmail.com>
2021-07-28 17:40:13 -0300

    Implementada a funcionalidade de comentários no post

---
+++ src/pages/api/preview.ts
@@ -0,0 +1,30 @@
+/* eslint-disable consistent-return */
+import { Document } from '@prismicio/client/types/documents';
+// import { Client } from '../../prismic-configuration';
+
+function linkResolver(doc: Document): string {
+  if (doc.type === 'posts') {
+    return `/post/${doc.uid}`;
+  }
+  return '/';
+}
+
+export default async (req, res): Promise<void> => {
+  const { token: ref, documentId } = req.query;
+  const redirectUrl = await Client(req)
+    .getPreviewResolver(ref, documentId)
+    .resolve(linkResolver, '/');
+
+  if (!redirectUrl) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  res.setPreviewData({ ref });
+
+  res.write(
+    `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${redirectUrl}" />
+    <script>window.location.href = '${redirectUrl}'</script>
+    </head>`
+  );
+  res.end();
+};
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -13,6 +13,7 @@ import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import { Comments } from '../../components/Comments';
 
 interface Post {
   first_publication_date: string | null;
@@ -88,6 +89,7 @@ export default function Post({ post }: PostProps): JSX.Element {
             </div>
           ))}
         </article>
+        <Comments />
       </main>
     </>
   );
